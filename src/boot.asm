04e0f6bf5894236f51ad5d800c2f16ee53b33bac
keason7 <keason7@protonmail.com>
2025-09-01 21:11:52 +0200

    Enable 32-bit protected mode after real mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,73 +1,124 @@
-; set code as 16 bits
-[BITS 16]
-
 ; cpu starts in real mode and bios loads this code at address 0x7c00
 ; set the origin at adress 0x7C00
 ; all labels and addresses are relative to this
 [ORG 0x7C00]
+
+; set code as 16 bits
+[BITS 16]
+
+; 0x08: points to the 2nd entry in the GDT table (the code segment descriptor)
+; 0x10: points to the 3rd entry in the GDT table (the data segment descriptor)
+OFFSET_CODE equ 0x08
+OFFSET_DATA equ 0x10
+
+; first enter real mode where the CPU has
+; less than 1 MB of RAM available for use
+real_mode:
     ; disable bios interrupts
     cli
-
     ; clear ax which is a 16-bit register in the x86 cpu (ax = 0x0000)
     ; ax is composed of ah = high byte (bits 8–15) and al = low byte (bits 0–7)
     xor ax, ax
     ; sets the data segment register to 0x0000
     mov ds, ax
-
+    ; sets es = 0x0000
+    mov es, ax
     ; sets SS (stack segment) = 0
     mov ss, ax
     ; sets stack pointer (sp) to 0x7C00
     mov sp, 0x7C00
-
-    ; clear direction flag (df) which controls the behavior of string instructions
-    ; df=0 (CLD): si increments after each operation: string read/write goes forward.
-    ; df=1 (STD): si decrements: string goes backward.
-    cld
-
-    ; puts the value 0xB800 into ax
-    ; 0xB8000 = start of video memory in text mode (color mode)
-    mov ax, 0xB800
-    ; sets es = 0xB800
-    mov es, ax
-    ; with es = 0xB800 and di = 0, es:di points to the top-left character cell of the screen.
-    xor di, di
-
-    ; load source index register (16-bit) used for string operations
-    ; with the address of the message string
-    mov si, msg
-
-print_loop:
-    ; load one byte of previously moved string at [DS:SI] into al register
-    lodsb
-    ; check for null terminator (0) that signify the end of the string
-    or  al, al
-    ; if al is zero, go to .done flag
-    jz  .done
-
-    ; after lodsb, al holds the character
-    ; setting AH prepares ax to contain a 16-bit value where al = char and ah = attribute (color)
-    mov ah, 0x02
-
-    ; store string word: it stores the 16-bit AX into memory at es:di
-    ; then, because df = 0, it increments di by 2 (word size)
-    ; effect: write (character, attribute) to the VGA text buffer and move to the next screen cell
-    stosw
-
-    jmp print_loop
-
-.done:
     ; enable bios interrupts
     sti
 
-hang:
-    ; stops the CPU until the next external interrupt occurs (saves power)
-    hlt
 
-    jmp hang
+load_protected_mode:
+    ; disable bios interrupts
+    cli
 
-; ---------------- data ----------------
-; 0 = null terminator
-msg db 'Hi there', 0
+    ; read port 0x92 (system control port A)
+    in al, 0x92
+    ; set bit 1
+    or al, 00000010b
+    ; write back, now A20 is enabled
+    out 0x92, al
+
+    ; load the gdt register (gdtr) that point the gdt table 
+    lgdt [gdt_register]
+
+    ; copy control register bit 0 value in eax
+    mov eax, cr0
+    ; set it to 1
+    or al, 1
+    ; copy value to cr0 to enable protected mode
+    mov cr0, eax
+
+    ; load cs = OFFSET_CODE (the code segment from the GDT) and then execute protected_mode
+    ; this is necessary because after setting cr0, the CPU is in protected mode 
+    ; but still using the old real mode cs
+    jmp OFFSET_CODE:protected_mode
+
+
+; setup basic flat model (GDT table)
+; a null descriptor
+; a code segment (r - x) that take all RAM space
+; a data segment (r w -) that take all RAM space
+; both code and data segments overlaps in available memory
+gdt_start:
+    ; null descriptor (8 bytes)
+    dd 0x0
+    dd 0x0
+
+    ; code segment
+    dw 0xFFFF ; limit (16 bits, 4 GiB address space in 32 bit mode)
+    dw 0x0000 ; base (16 bits)
+    db 0x00 ; base (8 bits)
+    db 10011010b ; access byte (8 bits, ring 0)
+    db 11001111b ; flags (4 bits) + limit (4 bits)
+    db 0x00 ; base (8 bits)
+
+    ; data segment
+    dw 0xFFFF ; limit (16 bits, 4 GiB address space in 32 bit mode)
+    dw 0x0000 ; base (16 bits)
+    db 0x00 ; base (8 bits)
+    db 10010010b ; access byte (8 bits, ring 0)
+    db 11001111b ; flags (4 bits) + limit (4 bits)
+    db 0x00 ; base (8 bits)
+
+
+gdt_end:
+
+
+; GDT descriptor that point GDT table with table adress and size
+gdt_register:
+    dw gdt_end - gdt_start - 1 ; size (limit)
+    dd gdt_start ; base address
+
+
+; set code as 32 bits
+[BITS 32]
+
+; enter protected mode where the CPU has
+; a maximum of 4GB of RAM
+; enables the system to enforce memory, hardware I/O protection and instruction via rings
+protected_mode:
+    ; load the data segment selector (0x10) into AX
+    mov ax, OFFSET_DATA
+    ; set ds, es, fs, ss, gs to our flat data segment
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov ss, ax
+    mov gs, ax
+
+    ; set base pointer and stack pointer to 0x9C00 which is far enough
+    ; from 0x7C00 to avoid overlap
+    ; stack grows down from higher adresses to lower (RAM is lower to higher)
+    mov ebp, 0x9C00        
+    mov esp, ebp
+
+    ; infinite loop
+    jmp $
+
 
 ; fill remaining bytes -2  with zeros
 ; $ = current address, $$ = start of section (0x7C00)
